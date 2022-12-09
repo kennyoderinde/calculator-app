ed32738eb660af798edc65d179d5d8ea9ce26729
Kehinde Oderinde <kennyoderyndeh@gmail.com>
2022-12-09 14:25:52 +0100

    This is a React ptroject, created from an api data

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import DtaFetching from './components/DtaFetching';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DtaFetching />
+      
+    </div>
+    
+  );
+}
+
+export default App;
